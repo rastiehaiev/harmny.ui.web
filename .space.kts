0e0cf7ce220eb81c7dda205ffcb2557e95a4d450
Roman Rastiehaiev <roman.rastiehaiev@gmail.com>
2024-02-03 21:09:43 +0200

    Change app icon.

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("Publish frontend distribution") {
     startOn {
         gitPush {
             anyBranchMatching {
-                +"refs/heads/HARMNY-T-64"
+                +"refs/heads/main"
             }
         }
     }
